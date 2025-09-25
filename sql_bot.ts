7e49f8b98abca303fa9884ee29d345aacbb133a3
MaxThePrisberry <bodomo9999@gmail.com>
2025-09-24 18:26:15 -0600

    Implement supabase query function

--- sql_bot.ts
+++ sql_bot.ts
@@ -1,4 +1,4 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";
 import promptSync from "prompt-sync";
 import { createClient } from "@supabase/supabase-js";
 
@@ -11,16 +11,18 @@ const supabaseKey = process.env.SUPABASE_API_KEY!;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function main() {
-  /* const { data, error } = await supabase
-    .from("testingtable")
-    .select("*");
-  console.log(data);
-  const question = prompt("What message you got for me?\n", "Print out just the word 'banana'.");
+  const question = prompt("Enter a question: ", "Print out just the word 'banana'.");
   if (question) {
     await queryGemini(question);
-  } */
+  }
+  
+}
 
-    
+async function querySupabase(query : String) {
+  const { data, error } = await supabase.rpc("execute_select_query", {
+    query_string: query
+  });
+  return JSON.stringify(data[0].result);
 }
 
 async function queryGemini(question: string) {
